613e9e424dafd9c49ba58be91e82a291c8604935
Ryan Bethel <ryan.bethel@gmail.com>
2021-07-02 09:55:24 -0400

    some cleanup

--- begin/index.js
+++ begin/index.js
@@ -1,38 +1,14 @@
-const next = require("next");
-const express= require("express")
-const react = require('react')
-const get_dom = require('react-dom')
-const arc = require('@architect/functions')
+let next = require("next");
+let express= require("express")
+let serverless = require('serverless-http');
 
-//const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev:false });
-const handle = app.getRequestHandler();
-//const port = process.env.PORT || 3000;
-
-// exports.handler = arc.http.async(async () => {
-//   try {
-//     await app.prepare();
-//     const server = express();
-//     server.all("*", (req, res) => {
-//       return handle(req, res);
-//     });
-//     return server
-//   } catch (e) {
-//     console.error(e);
-//     process.exit(1);
-//   }
-// });
+let app = next({ dev:false });
+let handle = app.getRequestHandler();
 
 let server= express()
 
-//server.get('/', (req, res) => res.send('Hello World!'))
-//server.get('/cool', (req, res)=> res.send('very cool'))
 server.get("*", (req, res) => {
        return handle(req, res);
      });
-
-const { parse } = require('url');
-const serverless = require('serverless-http');
-//app.handle=handle
 
 module.exports.handler = serverless(server)
